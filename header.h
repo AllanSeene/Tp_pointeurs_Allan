3cadbab0a0067b2f420884470faf8fc1c5f5ad86
Allan <allan.seene@outlook.fr>
2025-01-17 15:01:45 +0100

    Étape_1

---
+++ header.h
@@ -0,0 +1,15 @@
+#ifndef Fct 
+#define Fct
+
+struct Menu
+{
+    const char *option;
+};
+struct Application
+{
+    const char *nom;
+    struct Menu *menus;
+    int nb_menus;
+};
+
+#endif
--- main.c
+++ main.c
@@ -1,34 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-void min_max(int tab[], int size, int*min, int *max)
-{
-    if (size < 1)
-    {
-        return;
-    }
-    *min= tab[0];
-    *max= tab[0];
-    for (int i = 0; i < size; i++)
-    {
-        if(*min>tab[i])
-        {
-            *min=tab[i];
-        }
-        if (*max<tab[i])
-        {
-            *max=tab[i];
-        }
-        
-    }
-    
-}
+#include "header.h"
 int main()
 {
-  int min = 0 ; 
-  int max = 0; 
-  int tableau []={12,2,8,-7,15,2};
-  min_max(tableau ,6,&min,&max);
-  printf("min=%d , max=%d\n", min, max);
+struct Menu photoMenus;
+photoMenus.option ="s";
+struct Application photoApp;
+photoApp.nom="a";
+photoApp.nb_menus=7;
+photoApp.menus;
   return 0;
 }
