ef269d840e765483031533451a977b147e86a31f
Allan <allan.seene@outlook.fr>
2025-01-17 14:05:42 +0100

    1er_test_pointeurs

--- main.c
+++ main.c
@@ -1,9 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
 int main()
 {
-const a= 4;
-a=5;
+int a=2;
+printf("%p", &a);
 
-
+int *p=&a;
+printf("%p \n", p);
+printf("%p \n", &p);
+*p=5;
+printf("%d \n", a);
 
 return 0;
 }
